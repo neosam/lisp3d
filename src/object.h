8c4d8f4917d52d2a93d5121e90d4820c6db945f7
Simon Goller <neosam@neosam-desktop.(none)>
2008-08-07 19:18:59 +0200

    Added mesh settings to object
    Every object can now store the vertices and faces it has.  So we need just
    one draw function in the whole engine and can parse every object.

--- src/object.h
+++ src/object.h
@@ -1,6 +1,9 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <SDL_opengl.h>
+#define OBJ_ENTRY 256
+
 /* Standard object */
 struct Object {
   /* Type of object */
@@ -20,10 +23,17 @@ struct Object {
   /* Events */
   char *onDraw, *onInit;
 
-  /* Draw variables */
+  /* Sizer variables */
   double width, height, depth;
   double minX, minY, minZ;
   double maxX, maxY, maxZ;
+
+  /* Mesh settings */
+  GLdouble *vertices;
+  GLdouble *color;
+  GLdouble *faces;
+  int entries, size;
+  
 }; 
 typedef struct Object Object;
 
